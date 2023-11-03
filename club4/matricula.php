39766256b0ccdaae7b2f02d9df8fc3f9e15196b6
Vitor <vitor.acad@hotmail.com>
2023-11-03 16:01:14 -0300

    ok

---
+++ club4/matricula.php
@@ -0,0 +1,1 @@
+<h1>FAÇA SUA MATRÍCULA</h1>
--- club4/topo.php
+++ club4/topo.php
@@ -18,6 +18,7 @@
                     <a href="?pg=aulas">PLANOS E AULAS</a>
                     <a href="?pg=galeria">GALERIA</a>
                     <a href="?pg=faleconosco">CONTATO</a>
+                    <a href="?pg=matricula">MATRÍCULA</a>
                 </div>
             </td>
         </thead>
