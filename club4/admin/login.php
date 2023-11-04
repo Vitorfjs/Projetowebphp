bad20bc57e6e33c45a1b0791bb6d269cfe85713a
Vitor <vitor.acad@hotmail.com>
2023-11-04 16:10:52 -0300

    create login.php

---
+++ club4/admin/login.php
@@ -0,0 +1,13 @@
+<h1>Login</h1>
+
+<form action="acesso2.php" method="post">
+    <label>Usuário:</label>
+    <input type="text" class="form-control" name="usuario" placeholder="Usuário">
+    <br>
+    <label>Senha:</label>
+    <input type="password" class="form-control" name="senha" placeholder="Senha">
+    <br>
+    <div style="text-decoration-color: white;">
+        <button style="background-color: black;" type="submit" name="Entrar">
+    </div>
+</form>
