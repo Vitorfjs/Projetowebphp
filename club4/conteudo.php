532e87f4e6a16ab25cd082ceb8ddb90fdcd051e6
rrafaum <rafaelqalmeid@gmail.com>
2023-11-12 17:42:53 -0300

    Versão 1.1
    Melhorias de CSS, não 100% completo e ainda falta fazer o registro de matrículas

--- club4/conteudo.php
+++ club4/conteudo.php
@@ -1,167 +1,183 @@
-<!--Ínicio da página-->
-<div>
-    <img src="./img/fundo-club4.jpg" alt="Fundo Club4">
-</div>
+<main class="conteudo">
+    
+
+    <!--Ínicio da página-->
+    <section class="hero1">
+        <div class="imgfundo">
+            <img src="./img/fundo-club4.jpg" alt="Fundo Club4">
+        </div>
+    </section>
+    
 
 
-<!--Porque nos escolher-->
-<div style="background-color: #0a0a0a; color: white;">    
-    <div align="center">
-        <p>PORQUE NOS ESCOLHER?</p>
-        <h2>AUMENTE SEUS LIMITES</h2>
-    </div>
+    <!--Porque nos escolher-->
+    <section class="escolher">    
+        <div class="esctit">
+            <p>PORQUE NOS ESCOLHER?</p>
+            <h2>AUMENTE SEUS LIMITES</h2>
+        </div>
 
-    <div>
-        <table style="color: white;">
-            <tbody>
-                <td>
-                    <div align="center">
-                        <img src="./img/bike.png" alt="Ícone Bike" width="75px">
-                        <h4>Equipamento Moderno</h4>
-                        <p>Temos equipamento modernos e de boa qualidade
-                            para melhor conforto e execução de
-                            seu exercício.
-                        </p>
-                    </div>
-                </td> 
-
-                <td>
-                    <div align="center">
-                        <img src="./img/suco.png" alt="Ícone Suco" width="75px">
-                        <h4>Plano nutricional saudável</h4>
-                        <p>Profissional capacitado para prestar um atendimento
-                            com experiência personalizada e desenvolver uma
-                            nutrição adequada para você.
-                        </p>
-                    </div>
-                </td>    
-
-                <td>
-                    <div align="center">
-                        <img src="./img/peso.png" alt="Ícone Peso" width="75px">
-                        <h4>Plano de treino profissional</h4>
-                        <p>Nossa equipe conta com profissionais altamente
-                            habilitados a fim de propor um treino específico ao
-                            seu objetivo.
-                        </p>
-                    </div>
-                </td>
-
-                <td>
-                    <div align="center">
-                        <img src="./img/batida.png" alt="Ícone Batida" width="75px">
-                        <h4>Exclusivo para suas necessidades</h4>
-                        <p>Ambiente confortável, seguro e com varidades de
-                            aulas, como: musculação, ritbox, dança
-                            e muito mais!
-                        </p>
-                    </div>
-                </td>    
-            </tbody>    
-        </table>
-    </div>
-</div>
+        <div class="proposta">
+            <div class="prop1">
+                <img src="./img/bike.png" alt="Ícone Bike">
+                <h4>Equipamento Moderno</h4>
+                <p>Temos equipamento modernos e de boa qualidade
+                    para melhor conforto e execução de
+                    seu exercício.
+                </p>
+            </div>
 
 
-<!--Benefícios de saúde-->
-<div style="background-color: #151515; color: white" align="center">
-    <h2>O QUE OFERECEMOS</h2>
 
-    <table style="color: white">
-        <tbody>
-            <td align="center">
-                <img src="./img/galeria/bodybuilding.jpg" alt="Homem forte" width="90%">
+            <div class="prop2">
+                <img src="./img/suco.png" alt="Ícone Suco">
+                <h4>Plano nutricional saudável</h4>
+                <p>Profissional capacitado para prestar um
+                    antedimento com experiência personalizada e desenvolver uma
+                    nutrição adequada para você.
+                </p>
+            </div>
+                     
+
+                    
+            <div class="prop3">
+                <img src="./img/peso.png" alt="Ícone Peso">
+                <h4>Plano de treino profissional</h4>
+                <p>Nossa equipe conta com profissionais altamente
+                    habilitados a fim de propor um treino específico ao
+                    seu objetivo.
+                </p>
+            </div>
+                    
+
+                    
+            <div class="prop4">
+                <img src="./img/batida.png" alt="Ícone Batida">
+                <h4>Exclusivo para suas necessidades</h4>
+                <p>Ambiente confortável, seguro e com varidades
+                    de aulas, como: musculação, ritbox, dança
+                    e muito mais!
+                </p>
+            </div>
+                    
+        </div>
+    </section>
+
+
+    <!--Benefícios de saúde-->
+    <section class="oferecido">
+        <h2>O QUE OFERECEMOS</h2>
+
+        <div class="beneficios">
+            <div class="ben1">
+                <img src="./img/galeria/bodybuilding.jpg" alt="Homem forte">
                 <h5>Body Building</h5>
-            </td>
-
-            <td align="center">
-                <img src="./img/galeria/ganhomuscular.jpg" alt="Mulheres treinando" width="90%">
-                <h5>Ganho Muscular</h5>
-            </td>
-
-            <td align="center">
-                <img src="./img/galeria/perdadepeso.jpg" alt="Mulher na esteira" width="90%">
-                <h5>Perda de Peso</h5>
-            </td>
-        </tbody>
-    </table>    
-</div>
+            </div>
 
 
-<!--Ofertas de aula-->
-<div align="center">
-    <div>
-        <h2>ALGUMAS DE NOSSAS AULAS</h2>
-    </div>
+                
+            <div class="ben2">
+                    <img src="./img/galeria/ganhomuscular.jpg" alt="Mulheres treinando">
+                    <h5>Ganho Muscular</h5>
+            </div>
 
 
-    <div>
-        <table>
-            <tbody>
-                <td align="center">
-                    <img src="./img/danca.jpg" alt="Dança">
-                    <h3>Dança</h3>
+                
+            <div class="ben3">
+                    <img src="./img/galeria/perdadepeso.jpg" alt="Mulher na esteira">
+                    <h5>Perda de Peso</h5>
+            </div>
+        </div>   
+    </section>
+
+
+    <!--Ofertas de aula-->
+    <section class="ofaulas">
+        
+            <h2>ALGUMAS DE NOSSAS AULAS</h2>
+        
+        <div class="exaulas">
+            <div class="exaula">
+                <img src="./img/danca.jpg" alt="Dança">
+                <h3>DANÇA</h3>
+                <div class="exaula" id="desc">
                     <p>Pra você manjar de todos os passinhos, as aulas de dança da Club4 são ministradas por professores licenciados.</p>
-                </td>
+                    <a href="?pg=aulas">Saiba Mais</a>
+                </div>
+            </div>
 
-                <td align="center">
-                    <img src="./img/funcional.jpg" alt="Funcional">
-                    <h3>Funcional</h3>
+            <div class="exaula">
+                <img src="./img/funcional.jpg" alt="Funcional">
+                <h3>Funcional</h3>
+                <div class="exaula" id="desc">
                     <p>Emagrecimento com ganho de condicionamento físico. Esse nosso treino de funcional coletivo de alta intensidade.</p>
-                </td>
+                    <a href="?pg=aulas">Saiba Mais</a>
+                </div>
+            </div>
 
-                <td align="center">
-                    <img src="./img/muaythai.jpg" alt="Muay Thai">
-                    <h3>Muay Thai</h3>
+            <div class="exaula">
+                <img src="./img/muaythai.jpg" alt="Muay Thai">
+                <h3>Muay Thai</h3>
+                <div class="exaula" id="desc1">
                     <p>Muay Thai é uma das inspirações para a aula de BodyCombat. Esse treino ainda trabalha todo o corpo.</p>
-                </td>
+                    <a href="?pg=aulas">Saiba Mais</a>
+                </div>
+            </div>
 
-                <td align="center">
-                    <img src="./img/temmais.png" alt="Tem mais!">
-                    <h3>Tem mais!</h3>
+            <div class="exaula">
+                <img src="./img/temmais.png" alt="Tem mais!" class="imgextra">
+                <h3>Tem mais!</h3>
+                <div class="exaula" id="desc2">
                     <p>Clique no botão abaixo e conheça todas as aulas, horários e treinos disponíveis na <strong>Club4</strong> para você!</p>
                     <a href="?pg=aulas">Quero conhecer!</a>
-                </td>
-            </tbody>
-        </table>
-    </div>
-</div>
+                </div>
+            </div>                
+        </div>
+    </section>
 
 
-<!--Imagem divisória-->
-<div>
-    <img src="./img/salvacao.jpg" alt="Aqui a salvação é individual!" width="100%">
-</div>
+    <!--Imagem divisória-->
+    <section class="hero2">
+        <div class="imgfundo2">
+            <img src="./img/salvacao.jpg" alt="Aqui a salvação é individual!" width="100%">
+        </div>
+    </section>
+    
 
-
-<!--Procurar unidade-->
-<div align="center">
-    <h2>TEM UMA CLUB4 PERTINHO DE VOCÊ</h2>
-    <p>Escolha seu estado</p>
-    <select name="estados">
-        <option value="Selecione">Selecione</option>
-        <option value="Bahia">BAHIA</option>
-        <option value="Ceará">CEARÁ</option>
-        <option value="Maranhão">MARANHÃO</option>
-        <option value="Paraíba">PARAÍBA</option>
-        <option value="Pernambuco">PERNAMBUCO</option>
-        <option value="RN">RIO GRANDE DO NORTE</option>
-    </select>
-</div>
-
-
-<!--App da academia-->
-<div align="center">
-    <h3>APP EXCLUSIVO PARA ALUNOS</h3>
-    <table>
-        <tbody>
-            <tr>Tenha na palma da mão seus treinos e progresso</tr><br>
-            <tr>Agenda de aulas</tr><br>
-            <tr>Ver os exercícios em vídeo</tr><br>
-            <tr>Assistir às aulas on-line onde e quando quiser</tr><br>
-            <tr>Informações cadastrais e avisos</tr>
-        </tbody>
-    </table>
-    <img src="./img/googleplay.png" alt="Baixar na PlayStore" width="200px">
-    <img src="./img/appstore.png" alt="Baixar na AppStore" width="185px">
-</div>
+    <!--Procurar unidade-->
+    <section class="conectividade">
+        <div class="unidform">
+            <h2>TEM UMA CLUB4 PERTINHO DE VOCÊ</h2>
+            <p>Escolha seu estado</p>
+            <select name="estados">
+                <option value="Selecione">Selecione</option>
+                <option value="Bahia">BAHIA</option>
+                <option value="Ceará">CEARÁ</option>
+                <option value="Maranhao">MARANHÃO</option>
+                <option value="Paraiba">PARAÍBA</option>
+                <option value="Pernambuco">PERNAMBUCO</option>
+                <option value="RN">RIO GRANDE DO NORTE</option>
+            </select>
+        </div>
+    
+    
+    <!--App da academia-->
+        <div class="app">
+            <h3>APP EXCLUSIVO PARA ALUNOS</h3>
+            <div class="listben">
+                <ul class="appbens">
+                    <li class="appben">Tenha na palma da mão seus treinos e progressos</li>
+                    <li class="appben">Agenda de aulas</li>
+                    <li class="appben">Ver os exercícios em vídeo</li>
+                    <li class="appben">Assistir às aulas on-line onde e quando quiser</li>
+                    <li class="appben">Informações cadastrais e avisos</li>
+                </ul>
+            </div>
+            
+            <div class="applojas">
+                <img src="./img/googleplay.png" alt="Baixar na PlayStore" width="200px">
+                <img src="./img/appstore.png" alt="Baixar na AppStore" width="185px">
+            </div>
+        </div>
+    </section>
+</main>
--- club4/index.php
+++ club4/index.php
@@ -1,16 +1,38 @@
 <!--Página Inicial-->
-<?php
+<!DOCTYPE html>
+<html lang=pt-br">
 
-    include_once("topo.php");
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="./css/styles.css" media="screen"/>
+    <link rel="stylesheet" type="text/css" href="./css/styles2.css" media="screen"/>
+    <link rel="icon" type="image/x-icon" href="./img/favicon.ico">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+    <title>Club4</title>
+</head>
 
-    if(empty($_SERVER["QUERY_STRING"])){
-        $var = "conteudo.php";
-        include_once("$var");
-    }else{
-        $pg = $_GET['pg'];
-        include_once("$pg.php");
-    }
+<body>
+    <?php
 
-    include_once("rodape.php");
+        include_once("topo.php");
 
-?>
+        if(empty($_SERVER["QUERY_STRING"])){
+            $var = "conteudo.php";
+            include_once("$var");
+        }else{
+            $pg = $_GET['pg'];
+            include_once("$pg.php");
+        }
+
+        include_once("rodape.php");
+
+    ?>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>   
+</body>
+
+</html>
